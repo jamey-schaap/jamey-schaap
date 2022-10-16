fa64425d103f0522716b69c58cd6475542e13a08
Jamey <jameyschaap06@gmail.com>
2022-10-16 21:44:46 +0200

    feat: initial commit

---
+++ components/logo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Text, useColorModeValue } from "@chakra-ui/react";
+import styled from "@emotion/styled";
+
+const LogoBox = () => styled.span`
+  font-weight: bold;
+  font-size: 18px;
+  display: inline-flex;
+  align-items: center;
+`;
