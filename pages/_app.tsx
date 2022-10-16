7c5ad73efde1afee56b4aff54223b54df45c17a1
Jamey <jameyschaap06@gmail.com>
2022-10-16 23:05:20 +0200

    chore(dep): added chakra-ui, emotion and framer-motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import "../styles/globals.css";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { Router } from "next/router";
+import { ReactElement, ReactNode } from "react";
+import { AnimatePresence } from "framer-motion";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function App({ Component, pageProps, router }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  return getLayout(
+    <AnimatePresence
+      mode="wait"
+      initial={true}
+      onExitComplete={() => {
+        if (typeof window !== "undefined") {
+          window.scrollTo({ top: 0 });
+        }
+      }}
+    >
+      <Component {...pageProps} key={router.route} />
+    </AnimatePresence>
+  );
 }
 
-export default MyApp;
+export default App;
